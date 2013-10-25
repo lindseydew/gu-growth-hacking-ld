5ca718b03255c8920691ceebed6a2cefa6486c52
lindseydew <lindsey.dew@guardian.co.uk>
2013-10-25 14:27:23 +0100

    move file directory and add endpoint to hit content api

--- formats.py
+++
@@ -1,5 +0,0 @@
-
-def jsonp(request, json_string):
-	if request.get("callback"):
-		return "%s(%s)" % (request.get("callback"), json_string)
-	return json_string
--- models.py
+++
@@ -1,5 +0,0 @@
-from google.appengine.ext import ndb
-
-class Configuration(ndb.Model):
-	key = ndb.StringProperty()
-	value = ndb.StringProperty()
